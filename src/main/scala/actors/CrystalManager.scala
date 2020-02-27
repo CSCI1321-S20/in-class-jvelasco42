69fa71f4acf8c5e0dd0ab2cc08c76de96ff30241
Justin Velasco <jvelasco@trinity.edu>
2020-02-27 10:27:53 -0600

    Finished off Crystal Growth.

--- src/main/scala/actors/CrystalManager.scala
+++ src/main/scala/actors/CrystalManager.scala
@@ -1,27 +1,38 @@
 package actors
 
-import scalafx.scene.image.WritableImage
 import akka.actor.Actor
+import scalafx.scene.image.WritableImage
 import akka.actor.Props
+import scalafx.scene.paint.Color
+import scalafx.application.Platform
 
 class CrystalManager(img: WritableImage) extends Actor {
+  val writer = img.pixelWriter
+  val reader = img.pixelReader.get
+  for (i <- 0 until img.width.value.toInt) writer.setColor(i, img.height.value.toInt - 1, Color.Black)
+
   for (_ <- 1 to 20) {
     context.actorOf(Props[FloatyBit])
   }
-  for(child <- context.children) {
-    child ! FloatyBit.Move(400, 0)
+  for (child <- context.children) {
+    child ! FloatyBit.Move((util.Random.nextGaussian * 100 + 400).toInt min 799 max 0, 0)
   }
 
   import CrystalManager._
   def receive = {
-    case TestMove(x, y, oldX, oldY) => 
+    case TestMove(x, y, ox, oy) =>
       if (x < 0 || x >= img.width.value || y < 0 || y >= img.height.value) {
-        sender ! FloatyBit.Move(oldX, oldY)
+        sender ! FloatyBit.Move(ox, oy)
+      } else if (reader.getColor(x, y) == Color.Black) {
+        Platform.runLater(writer.setColor(ox, oy, Color.Black))
+        sender ! FloatyBit.Move((util.Random.nextGaussian * 100 + 400).toInt min 799 max 0, 0)
+      } else {
+        sender ! FloatyBit.Move(x, y)
       }
-    case m => println("Unhandled message in CrystalManager: " + m)
+    case m => println("Unhandled message in CrystalManager: "+m)
   }
 }
 
 object CrystalManager {
-  case class TestMove(x: Int, y: Int, oldX: Int, oldY: Int)
+  case class TestMove(x: Int, y: Int, ox: Int, oy: Int)
 }
